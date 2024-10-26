8cd4907b13839fbe992b11e26a12d0252a4e56eb
titanforged1 <sebnemozeski@gmail.com>
2024-10-26 15:28:36 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
+//tepmlate engine
+app.set('view engine', 'ejs');
 
 //middlewares
 app.use(express.static('public'));
-
+//routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
